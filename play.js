59cbad812cc4d73a344f61221fb3985270c8ef73
DevHalpin <echidnablade@hotmail.com>
2020-08-26 20:00:27 +0000

    Created stdin input functions for controlling snake

--- play.js
+++ play.js
@@ -1,3 +1,20 @@
 const {connect} = require('./client');
+
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  const handleUserInput = (key) => {
+    if (key === '/u0003') {
+      process.exit
+    }
+  }
+  stdin.on('data', (key) => {
+    handleUserInput(key);
+  })
+  return stdin;
+}
+
 console.log('Connecting ...');
 connect();
