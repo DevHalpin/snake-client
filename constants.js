8f0d8c411fdadf423453ed2cfdfc2661f7112159
DevHalpin <echidnablade@hotmail.com>
2020-08-26 21:11:01 +0000

    Updated input to use constants for movement and talking

---
+++ constants.js
@@ -0,0 +1,21 @@
+const IP = '135.23.222.131';
+const PORT = 50542;
+
+module.exports = {
+  IP,
+  PORT,
+  MOVE_UP_KEY : 'w',
+  MOVE_DOWN_KEY : 's',
+  MOVE_LEFT_KEY : 'a',
+  MOVE_RIGHT_KEY : 'd',
+  SAY_1_KEY: 'r',
+  SAY_2_KEY: 't',
+  MAPPINGS : {
+    MOVE_UP_KEY : 'Move: up',
+    MOVE_DOWN_KEY : 'Move: down',
+    MOVE_LEFT_KEY : 'Move: left',
+    MOVE_RIGHT_KEY : 'Move: right',
+    SAY_1_KEY: 'Say: I\'m gonna die!',
+    SAY_2_KEY: 'Say: Sup',
+  }
+};
--- input.js
+++ input.js
@@ -1,4 +1,5 @@
 let connection;
+const constants = require('./constants')
 
 const setupInput = (conn) => {
   connection = conn;
@@ -15,14 +16,13 @@ const setupInput = (conn) => {
 const handleUserInput = (key) => {
   if (key === '\u0003') {
     process.exit();
-  } else if (key === 'w') {
-    connection.write('Move: up');
-  } else if (key === 's') {
-    connection.write('Move: down');
-  } else if (key === 'a') {
-    connection.write('Move: left');
-  } else if (key === 'd') {
-    connection.write('Move: right');
+  }
+  else {
+    for (cKey in constants) {
+      if (constants[cKey] === key) {
+        connection.write(constants.MAPPINGS[cKey])
+      }
+    }
   }
 }
 
