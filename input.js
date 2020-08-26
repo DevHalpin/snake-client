09699364afd22d4407de512b11bc02a62f8ee7ad
DevHalpin <echidnablade@hotmail.com>
2020-08-26 20:27:46 +0000

    Updated input with actual movement commands to server

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = () => {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -10,8 +13,16 @@ const setupInput = () => {
 }
 
 const handleUserInput = (key) => {
-  if (key === '/u0003') {
-    process.exit
+  if (key === '\u0003') {
+    process.exit();
+  } else if (key === 'w') {
+    connection.write('Move: up');
+  } else if (key === 's') {
+    connection.write('Move: down');
+  } else if (key === 'a') {
+    connection.write('Move: left');
+  } else if (key === 'd') {
+    connection.write('Move: right');
   }
 }
 
--- play.js
+++ play.js
@@ -3,5 +3,5 @@ const { setupInput } = require('./input');
 
 console.log('Connecting ...');
 connect();
-
-setupInput();
+let connection = connect()
+setupInput(connection);
